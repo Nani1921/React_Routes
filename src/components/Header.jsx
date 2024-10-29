5bd071f7d71756d81e2383687f7ef470ecdb427c
Nani1921 <ramireddy1203@gmail.com>
2024-10-29 11:04:58 +0530

    first commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -37,7 +37,7 @@ const Header = () => {
           </li>
           <li>
             <NavLink
-              to="/user"
+              to="/users"
              
             >
               Users
